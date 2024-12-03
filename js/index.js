b9603dffd23e4cc662654612266dd15d6c7c4d7b
AKHIL KUMAR TS <ak2072719@gmail.com>
2024-12-03 16:54:41 +0530

    updated

---
+++ js/index.js
@@ -0,0 +1,98 @@
+// const a=10
+// const b=10.5
+// const c=[1,2,3,4,5]
+// const d={name:'anu',age:20}
+// const e=true
+// console.log(typeof e);
+// let e
+// console.log(e);
+
+// let a=10
+// let b=20
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// a++
+// ++a
+// --a
+// a--
+// console.log(a);
+
+// let a=10
+// a+=10
+// a-=10
+// a*=10
+// a%=10
+// a/=10
+// a**=10
+// console.log(a);
+
+
+// console.log(5==5);
+// console.log(5=="5");
+// console.log(5==="5");
+// console.log(5!="5");
+// console.log(5!=5);
+// console.log(5!=="5");
+
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=2);
+// console.log(2>=5);
+// console.log(2<=2);
+// console.log(2<=1);
+
+// console.log(10==10 && 5==5);
+// console.log(10==5 && 5==10);
+// console.log(10==10 || 2==5);
+// console.log(10==5 || 5==10);
+// console.log(10==10 != 10==10);
+
+
+function sample(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    result=document.getElementById("res").innerHTML=a+b
+    console.log(a+b);
+}
+function sample1(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    result=document.getElementById("res").innerHTML=a-b
+    console.log(a-b);
+}
+function sample2(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    result=document.getElementById("res").innerHTML=a*b
+    console.log(a*b);
+}
+function sample3(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    result=document.getElementById("res").innerHTML=a/b
+    console.log(a/b);
+}
+function sample4(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    result=document.getElementById("res").innerHTML=a%b
+    console.log(a%b);
+}
+function sample5(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    result=document.getElementById("res").innerHTML=a**b
+    console.log(a**b);
+}
+
+
+
+
+
+
+
+
